f5ce271b0c7a5db3d09abde2bc63ae2173aa0040
cateland <axel.cateland@iadvize.com>
2019-05-27 18:01:02 +0200

    feat(apply): implem, doc, tests

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Functor2 } from 'fp-ts/lib/Functor';
+import { Apply2 } from 'fp-ts/lib/Apply';
 
 declare module 'fp-ts/lib/HKT' {
   interface URI2HKT2<L, A> {
@@ -19,32 +20,112 @@ export type RemoteData<L, A> =
 export class NotAsked<L, A> {
   readonly _tag = 'NotAsked';
   constructor() {}
-  map<B>(f: (value: A) => B): RemoteData<L, B> {
+  isNotAsked() {
+    return true;
+  }
+  isLoading() {
+    return false;
+  }
+  isFailure() {
+    return false;
+  }
+  isSuccess() {
+    return false;
+  }
+
+  map<B>(f: (a: A) => B): RemoteData<L, B> {
     return this as any;
+  }
+  ap<B>(fab: RemoteData<L, (a: A) => B>): RemoteData<L, B> {
+    if (fab.isFailure()) {
+      return fab as any;
+    }
+    return (fab.isNotAsked() ? fab : this) as any;
   }
 }
 
 export class Loading<L, A> {
   readonly _tag = 'Loading';
   constructor() {}
-  map<B>(f: (value: A) => B): RemoteData<L, B> {
+
+  isNotAsked() {
+    return false;
+  }
+  isLoading() {
+    return true;
+  }
+  isFailure() {
+    return false;
+  }
+  isSuccess() {
+    return false;
+  }
+
+  map<B>(f: (a: A) => B): RemoteData<L, B> {
     return this as any;
+  }
+  ap<B>(fab: RemoteData<L, (a: A) => B>): RemoteData<L, B> {
+    if (fab.isNotAsked()) {
+      return notAsked();
+    }
+    if (fab.isFailure()) {
+      return fab as any;
+    }
+    return (fab.isLoading() ? fab : this) as any;
   }
 }
 
 export class Failure<L, A> {
   readonly _tag = 'Failure';
   constructor(readonly value: L) {}
+
+  isNotAsked() {
+    return false;
+  }
+  isLoading() {
+    return false;
+  }
+  isFailure() {
+    return true;
+  }
+  isSuccess() {
+    return false;
+  }
+
   map<B>(f: (a: A) => B): RemoteData<L, B> {
-    return new Failure(this.value);
+    return this as any;
+  }
+  ap<B>(fab: RemoteData<L, (a: A) => B>): RemoteData<L, B> {
+    return (fab.isFailure() ? fab : this) as any;
   }
 }
 
 export class Success<L, A> {
   readonly _tag = 'Success';
   constructor(readonly value: A) {}
+
+  isNotAsked() {
+    return false;
+  }
+  isLoading() {
+    return false;
+  }
+  isFailure() {
+    return false;
+  }
+  isSuccess() {
+    return true;
+  }
+
   map<B>(f: (a: A) => B): RemoteData<L, B> {
     return new Success(f(this.value));
+  }
+  ap<B>(fab: RemoteData<L, (a: A) => B>): RemoteData<L, B> {
+    if (fab.isSuccess()) {
+      const _fab = fab as Success<L, (a: A) => B>;
+      return this.map(_fab.value);
+    }
+    return fab as any;
   }
 }
 
@@ -61,10 +142,36 @@ export const success = <L, A>(a: A): RemoteData<L, A> => {
   return new Success(a);
 };
 
+const ap = <L, A, B>(
+  fab: RemoteData<L, (a: A) => B>,
+  fa: RemoteData<L, A>,
+): RemoteData<L, B> => fa.ap(fab);
+
 const map = <L, A, B>(fa: RemoteData<L, A>, f: (a: A) => B): RemoteData<L, B> =>
   fa.map(f);
 
-export const remoteData: Functor2<URI> = {
+export const map2 = <L, A, B, C>(
+  f: (a: A) => (b: B) => C,
+  fa: RemoteData<L, A>,
+  fb: RemoteData<L, B>,
+): RemoteData<L, C> => ap(fa.map(f), fb);
+
+export const map3 = <L, A, B, C, D>(
+  f: (a: A) => (b: B) => (c: C) => D,
+  fa: RemoteData<L, A>,
+  fb: RemoteData<L, B>,
+  fc: RemoteData<L, C>,
+): RemoteData<L, D> => ap(ap(fa.map(f), fb), fc);
+
+export const append = <L, A, B>(
+  fa: RemoteData<L, A>,
+  fb: RemoteData<L, B>,
+): RemoteData<L, [A, B]> => {
+  return ap(fa.map((a: A) => (b: B): [A, B] => [a, b]), fb);
+};
+
+export const remoteData: Functor2<URI> & Apply2<URI> = {
   URI,
+  ap,
   map,
 };
--- test/remotedata.test.ts
+++ test/remotedata.test.ts
@@ -1,4 +1,13 @@
-import { remoteData, notAsked, loading, failure, success } from '../src/main';
+import {
+  remoteData,
+  notAsked,
+  loading,
+  failure,
+  success,
+  map2,
+  map3,
+  append,
+} from '../src/main';
 
 describe('RemoteData', () => {
   it('map', () => {
@@ -14,5 +23,118 @@ describe('RemoteData', () => {
       failure('xyz'),
     );
     expect(remoteData.map(success('abc'), f)).toEqual(success(3));
+  });
+
+  describe('ap', () => {
+    const f = (s: string): number => s.length;
+    it('return failure if any of the RemoteData fail', () => {
+      expect(
+        notAsked<string, string>().ap(
+          failure<string, (s: string) => number>('xyz'),
+        ),
+      ).toEqual(failure('xyz'));
+      expect(
+        loading<string, string>().ap(
+          failure<string, (s: string) => number>('xyz'),
+        ),
+      ).toEqual(failure('xyz'));
+      expect(success('abc').ap(failure('xyz'))).toEqual(failure('xyz'));
+      expect(
+        failure<string, string>('xyz').ap(
+          notAsked<string, (s: string) => number>(),
+        ),
+      ).toEqual(failure('xyz'));
+      expect(
+        failure<string, string>('xyz').ap(
+          loading<string, (s: string) => number>(),
+        ),
+      ).toEqual(failure('xyz'));
+      expect(
+        failure<string, string>('xyz').ap(
+          success<string, (s: string) => number>(f),
+        ),
+      ).toEqual(failure('xyz'));
+    });
+
+    it('return left failure if two RemoteData are failed', () => {
+      expect(
+        failure<string, string>('xyz').ap(
+          failure<string, (s: string) => number>('stu'),
+        ),
+      ).toEqual(failure('stu'));
+    });
+
+    it('return Loading if both RemoteData are loading or one is a success', () => {
+      expect(
+        loading<string, string>().ap(loading<string, (s: string) => number>()),
+      ).toEqual(loading());
+      expect(
+        loading<string, string>().ap(success<string, (s: string) => number>(f)),
+      ).toEqual(loading());
+      expect(success('abc').ap(loading())).toEqual(loading());
+    });
+
+    it('return NotAsked if one of the RemoteData is NotAsked and the other is not Failed', () => {
+      expect(
+        notAsked<string, string>().ap(
+          notAsked<string, (s: string) => number>(),
+        ),
+      ).toEqual(notAsked());
+      expect(
+        notAsked<string, string>().ap(loading<string, (s: string) => number>()),
+      ).toEqual(notAsked());
+      expect(
+        notAsked<string, string>().ap(
+          success<string, (s: string) => number>(f),
+        ),
+      ).toEqual(notAsked());
+      expect(
+        loading<string, string>().ap(notAsked<string, (s: string) => number>()),
+      ).toEqual(notAsked());
+      expect(success('abc').ap(notAsked())).toEqual(notAsked());
+      expect(notAsked<string, string>().ap(success(f))).toEqual(notAsked());
+    });
+
+    it('return a RemoteData with an object of values from two succesfull RemoteData', () => {
+      expect(success('abc').ap(success(f))).toEqual(success(3));
+    });
+
+    describe('map2', () => {
+      const f = (s1: string) => (s2: string) => `${s1}${s2}`;
+      it('return success if both remoteData are success', () => {
+        expect(map2(f, success('abc'), success('def'))).toEqual(
+          success('abcdef'),
+        );
+      });
+      it('return failure if one remoteData is a failure', () => {
+        expect(map2(f, success('abc'), failure('xyz'))).toEqual(failure('xyz'));
+      });
+    });
+
+    describe('map3', () => {
+      const f = (s1: string) => (s2: string) => (s3: string) =>
+        `${s1}${s2}${s3}`;
+      it('return success if all remoteData are success', () => {
+        expect(map3(f, success('abc'), success('def'), success('ghi'))).toEqual(
+          success('abcdefghi'),
+        );
+      });
+      it('return failure if one remoteData is a failure', () => {
+        expect(map3(f, success('abc'), failure('xyz'), success('ghi'))).toEqual(
+          failure('xyz'),
+        );
+      });
+    });
+
+    describe('append', () => {
+      it('return success if both remoteData are success', () => {
+        expect(append(success('abc'), success('def'))).toEqual(
+          success(['abc', 'def']),
+        );
+      });
+      it('return failure if one remoteData is a failure', () => {
+        expect(append(success('abc'), failure('xyz'))).toEqual(failure('xyz'));
+      });
+    });
   });
 });
